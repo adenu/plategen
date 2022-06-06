8457d9002525a1e59d7a444037ff97ed903a339a
netomartins <neto.martins@estacio.br>
2022-06-06 00:37:19 -0300

    uma função só

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Converte o txt para json
 const fs = require("fs");
-const raw = fs.readFileSync("palavras.txt").toString();
+const raw = fs.readFileSync("./palavras.txt").toString().replace('\r','');
 const data = raw.split("\n");
 const headers = data.shift().split("|");
 let json = [];
@@ -15,18 +15,22 @@ for (let i = 0; i < data.length; i++) {
 
 // Filtros
 let filtroTamanho = [];
-let filtroTerceiroChar = [];
-let filtroQuintoChar = [];
-let filtroSextoChar = [];
+let filtroChars = [];
 
 json.forEach((entrada) => {
   let tamanhoPalavra = entrada.palavra.length;
-  if (tamanhoPalavra === 7) {
+  entrada.palavra = entrada.palavra.replace('\r', '')
+  if (tamanhoPalavra === 8) {
     entrada.palavra = entrada.palavra.replace("ç", "c");
     entrada.palavra = entrada.palavra.replace("ã", "a");
+    entrada.palavra = entrada.palavra.replace("á", "a");
     entrada.palavra = entrada.palavra.replace("é", "e");
     entrada.palavra = entrada.palavra.replace("ê", "e");
     entrada.palavra = entrada.palavra.replace("í", "i");
+    entrada.palavra = entrada.palavra.replace("ó", "o");
+    entrada.palavra = entrada.palavra.replace("ô", "o");
+    entrada.palavra = entrada.palavra.replace("õ", "o");
+    entrada.palavra = entrada.palavra.replace("ú", "u");
     filtroTamanho.push(entrada.palavra);
   }
 });
@@ -35,81 +39,65 @@ filtroTamanho.forEach((entrada) => {
   let terceiroCaracter = entrada.charAt(3).toLowerCase();
   if (terceiroCaracter == "i") {
     entrada = entrada.substring(0, 3) + "1" + entrada.substring(3 + 1);
-    filtroTerceiroChar.push(entrada);
   } else if (terceiroCaracter == "e") {
     entrada = entrada.substring(0, 3) + "3" + entrada.substring(3 + 1);
-    filtroTerceiroChar.push(entrada);
   } else if (terceiroCaracter == "a") {
     entrada = entrada.substring(0, 3) + "4" + entrada.substring(3 + 1);
-    filtroTerceiroChar.push(entrada);
   } else if (terceiroCaracter == "s") {
     entrada = entrada.substring(0, 3) + "5" + entrada.substring(3 + 1);
-    filtroTerceiroChar.push(entrada);
   } else if (terceiroCaracter == "b") {
     entrada = entrada.substring(0, 3) + "6" + entrada.substring(3 + 1);
-    filtroTerceiroChar.push(entrada);
   } else if (terceiroCaracter == "t") {
     entrada = entrada.substring(0, 3) + "7" + entrada.substring(3 + 1);
-    filtroTerceiroChar.push(entrada);
   } else if (terceiroCaracter == "o") {
     entrada = entrada.substring(0, 3) + "0" + entrada.substring(3 + 1);
-    filtroTerceiroChar.push(entrada);
   }
-});
 
-filtroTerceiroChar.forEach((entrada) => {
   let quintoCaracter = entrada.charAt(5).toLowerCase();
   if (quintoCaracter == "i") {
     entrada = entrada.substring(0, 5) + "1" + entrada.substring(5 + 1);
-    filtroQuintoChar.push(entrada);
   } else if (quintoCaracter == "e") {
     entrada = entrada.substring(0, 5) + "3" + entrada.substring(5 + 1);
-    filtroQuintoChar.push(entrada);
   } else if (quintoCaracter == "a") {
     entrada = entrada.substring(0, 5) + "4" + entrada.substring(5 + 1);
-    filtroQuintoChar.push(entrada);
   } else if (quintoCaracter == "s") {
     entrada = entrada.substring(0, 5) + "5" + entrada.substring(5 + 1);
-    filtroQuintoChar.push(entrada);
   } else if (quintoCaracter == "b") {
     entrada = entrada.substring(0, 5) + "6" + entrada.substring(5 + 1);
-    filtroQuintoChar.push(entrada);
   } else if (quintoCaracter == "t") {
     entrada = entrada.substring(0, 5) + "7" + entrada.substring(5 + 1);
-    filtroQuintoChar.push(entrada);
   } else if (quintoCaracter == "o") {
     entrada = entrada.substring(0, 5) + "0" + entrada.substring(5 + 1);
-    filtroQuintoChar.push(entrada);
   }
-});
 
-filtroQuintoChar.forEach((entrada) => {
   let sextoCaracter = entrada.charAt(6).toLowerCase();
   if (sextoCaracter == "i") {
     entrada = entrada.substring(0, 6) + "1" + entrada.substring(6 + 1);
-    filtroSextoChar.push(entrada.toUpperCase());
+    filtroChars.push(entrada.toUpperCase());
   } else if (sextoCaracter == "e") {
     entrada = entrada.substring(0, 6) + "3" + entrada.substring(6 + 1);
-    filtroSextoChar.push(entrada.toUpperCase());
+    filtroChars.push(entrada.toUpperCase());
   } else if (sextoCaracter == "a") {
     entrada = entrada.substring(0, 6) + "4" + entrada.substring(6 + 1);
-    filtroSextoChar.push(entrada.toUpperCase());
+    filtroChars.push(entrada.toUpperCase());
   } else if (sextoCaracter == "s") {
     entrada = entrada.substring(0, 6) + "5" + entrada.substring(6 + 1);
-    filtroSextoChar.push(entrada.toUpperCase());
+    filtroChars.push(entrada.toUpperCase());
   } else if (sextoCaracter == "b") {
     entrada = entrada.substring(0, 6) + "6" + entrada.substring(6 + 1);
-    filtroSextoChar.push(entrada.toUpperCase());
+    filtroChars.push(entrada.toUpperCase());
   } else if (sextoCaracter == "t") {
     entrada = entrada.substring(0, 6) + "7" + entrada.substring(6 + 1);
-    filtroSextoChar.push(entrada.toUpperCase());
+    filtroChars.push(entrada.toUpperCase());
   } else if (sextoCaracter == "o") {
     entrada = entrada.substring(0, 6) + "0" + entrada.substring(6 + 1);
-    filtroSextoChar.push(entrada.toUpperCase());
+    filtroChars.push(entrada.toUpperCase());
   }
+
 });
 
-const listaFinal = JSON.stringify(filtroSextoChar);
+
+const listaFinal = JSON.stringify(filtroChars);
 
 function writeNewFile() {
   fs.writeFile("./lista-final.json", listaFinal, "utf8", function (err) {
@@ -122,4 +110,4 @@ function writeNewFile() {
 }
 
 // Reescrever arquivo lista-final
-// writeNewFile();
+writeNewFile();
--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
+
+function randomizeNumbers() {
 fetch("./lista-final.json")
   .then(response => {
     return response.json();
   })
   .then(data => {
-    let placa = document.body.getElementById('placa');
+    let numeroPlaca = document.querySelector('span#numero-placa');
+    randomPlaca = data[Math.floor(Math.random() * data.length)]
+    randomPlaca = randomPlaca.substring(0, 3) + " " + randomPlaca.substring(2 + 1);
+    numeroPlaca.innerHTML = randomPlaca;
   });
+};
+
+randomizeNumbers();
