3bdabaf53e1666b8bb143c6e862f8eb2392e30be
netomartins <neto.martins@estacio.br>
2022-06-06 03:21:17 -0300

    traduzir placa

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # plategen
 Gerador de placas de automóveis no padrão Mercosul, utilizando a sequencia correta de números e letras para formar palavras ou expressões possíveis e interessantes.
+
+## Resources
+Utilizei uma lista de palavras disponível pelo IME/USP.
--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,37 @@
 
-function randomizeNumbers() {
+const numeroPlaca = document.querySelector('span#numero-placa');
+const possibleLetters = ["i", "e", "a", "s", "b", "t", "o"];
+const possibleNumbers = ["1", "3", "4", "5", "6", "7", "0"];
+let normalWord;
+
+const randomizeNumbers = () => {
 fetch("./lista-final.json")
   .then(response => {
     return response.json();
   })
   .then(data => {
-    let numeroPlaca = document.querySelector('span#numero-placa');
     randomPlaca = data[Math.floor(Math.random() * data.length)]
     randomPlaca = randomPlaca.substring(0, 3) + " " + randomPlaca.substring(2 + 1);
     numeroPlaca.innerHTML = randomPlaca;
+    normalWord = randomPlaca;
+    numeroPlaca.setAttribute('original', randomPlaca);
+
+    possibleNumbers.forEach(number => {
+      let letter = possibleLetters[possibleNumbers.indexOf(number)];
+      normalWord = normalWord.replaceAll(number, letter.toUpperCase());
+    })
+    numeroPlaca.setAttribute('translated', normalWord.replace(' ', ''));
   });
 };
 
 randomizeNumbers();
+
+const replaceNumbers = () => {
+
+  if (numeroPlaca.innerHTML == numeroPlaca.getAttribute('translated')) {
+    numeroPlaca.innerHTML = numeroPlaca.getAttribute('original');
+  } else {
+    numeroPlaca.innerHTML = numeroPlaca.getAttribute('translated');
+  }
+}
+
